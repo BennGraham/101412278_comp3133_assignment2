62e24960aafd820d789f64ebec2b61d1feffb7f2
Benn <benngraham90@gmail.com>
2025-04-05 14:45:07 -0400

    display employees in angular material table component

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -4,16 +4,44 @@ import { Employee } from "../models/employee";
 import { CommonModule } from "@angular/common";
 import { MatCardModule } from "@angular/material/card";
 import { AvatarComponent } from "../avatar/avatar.component";
+import { MatTableModule } from "@angular/material/table";
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
 
 @Component({
   selector: "app-employee",
-  imports: [CommonModule, MatCardModule, AvatarComponent],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    AvatarComponent,
+    MatTableModule,
+    MatIconModule,
+  ],
   templateUrl: "./employee.component.html",
   styleUrl: "./employee.component.css",
 })
 export class EmployeeComponent implements OnInit {
   employees: Employee[] = [];
   constructor(private graphqlService: GraphqlService) {}
+
+  displayedColumns: string[] = [
+    "first_name",
+    "last_name",
+    "designation",
+    "department",
+  ];
+
+  viewDetails(employee: Employee) {
+    console.log(employee);
+  }
+
+  editEmployee(employee: Employee) {
+    console.log("edit button");
+  }
+
+  deleteEmployee(employee: Employee) {
+    console.log("delete button");
+  }
 
   ngOnInit(): void {
     this.onLoad();
@@ -22,6 +50,7 @@ export class EmployeeComponent implements OnInit {
   private onLoad() {
     return this.graphqlService.getAllEmployees().subscribe({
       next: (employees) => {
+        console.log(employees);
         this.employees = employees;
       },
       error: (error) => {
