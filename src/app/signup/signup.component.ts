74e732b1d8caf2d7138bf239d70e96ea20998575
Benn <benngraham90@gmail.com>
2025-04-05 11:10:28 -0400

    toast notifications for signup success/failure, redirect to login after signup

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -10,7 +10,9 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { GraphqlService } from '../network/graphql.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -37,7 +39,11 @@ export class SignupComponent {
     ]),
   });
 
-  constructor(private graphqlService: GraphqlService) {}
+  constructor(
+    private graphqlService: GraphqlService,
+    private router: Router,
+    private snackbar: MatSnackBar
+  ) {}
 
   get username() {
     return this.signupForm.get('username');
@@ -61,10 +67,23 @@ export class SignupComponent {
         )
         .subscribe({
           next: (user) => {
-            console.log(user);
+            console.log('success! new user: ', user);
+            this.snackbar.open('Signup Successful! Please log in.', 'Close', {
+              duration: 3000,
+              horizontalPosition: 'right',
+              verticalPosition: 'bottom',
+              panelClass: ['success-snackbar'],
+            });
+            this.router.navigate(['/login']);
           },
           error: (error) => {
             console.error(error);
+            this.snackbar.open(`Signup Failed! ${error.message}`, 'Close', {
+              duration: 3000,
+              horizontalPosition: 'right',
+              verticalPosition: 'bottom',
+              panelClass: ['error-snackbar'],
+            });
           },
         });
     }
